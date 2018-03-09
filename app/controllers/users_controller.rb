571087018162fbeb21e367de4edbe786d24747ae
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-10 07:55:59 +0900

    着火点を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        SampleMailer.send_when_create(@user).deliver
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
@@ -36,7 +37,7 @@ class UsersController < ApplicationController
       end
     end
   end
-
+  
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
